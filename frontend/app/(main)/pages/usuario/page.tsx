461abc0ed1d050894744ba2035f1babd9615b38b
Marcos Fernando <marcos.fersh@gmail.com>
2025-06-23 00:07:12 -0300

    implementacao do useMemo do react
    implementacao do useMemo do react

--- frontend/app/(main)/pages/usuario/page.tsx
+++ frontend/app/(main)/pages/usuario/page.tsx
@@ -5,22 +5,16 @@ import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 import { Dialog } from 'primereact/dialog';
 import { FileUpload } from 'primereact/fileupload';
-import { InputNumber, InputNumberValueChangeEvent } from 'primereact/inputnumber';
 import { InputText } from 'primereact/inputtext';
-import { InputTextarea } from 'primereact/inputtextarea';
-import { RadioButton, RadioButtonChangeEvent } from 'primereact/radiobutton';
-import { Rating } from 'primereact/rating';
 import { Toast } from 'primereact/toast';
 import { Toolbar } from 'primereact/toolbar';
 import { classNames } from 'primereact/utils';
-import React, { useEffect, useRef, useState } from 'react';
-import { ProductService } from '../../../../demo/service/ProductService';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Projeto } from '@/types';
 import { UsuarioService } from '@/service/UsuarioService';
-import { error } from 'console';
 
-/* @todo Used 'as any' for types here. Will fix in next version due to onSelectionChange event type issue. */
-const Crud = () => {
+
+const Usuario = () => {
     let usuarioVazio: Projeto.Usuario = {
         id: 0,
         nome: '',
@@ -39,8 +33,7 @@ const Crud = () => {
     const [globalFilter, setGlobalFilter] = useState('');
     const toast = useRef<Toast>(null);
     const dt = useRef<DataTable<any>>(null);
-    const usuarioService = new UsuarioService();
-
+    const usuarioService = useMemo(() => new UsuarioService(), []);
 
     useEffect(() => {
         if (!usuarios) {
@@ -463,4 +456,4 @@ const Crud = () => {
     );
 };
 
-export default Crud;
+export default Usuario;
